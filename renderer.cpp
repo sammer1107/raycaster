0222e56f7db8f186189d66a26bcc90aa31d31c9e
sammer1107 <qscesz871107@gmail.com>
2020-10-26 14:44:03 +0800

    Improve wall rendering

--- renderer.cpp
+++ renderer.cpp
@@ -24,20 +24,35 @@ void Renderer::TraceFrame(Game *g, uint32_t *fb)
             ws = 0;
             sso = HORIZON_HEIGHT;
         }
-        uint16_t to = tso;
-        uint16_t ts = tst;
 
         for (int y = 0; y < ws; y++) {
             *lb = GetARGB(96 + (HORIZON_HEIGHT - y));
             lb += SCREEN_WIDTH;
         }
 
-        for (int y = 0; y < sso * 2; y++) {
-            // paint texture pixel
+        uint16_t to = (32 << 10) - (tst >> 1);
+        lb += (sso - 1) * SCREEN_WIDTH;
+        for (int y = 0; y < sso; y++) {
             auto ty = static_cast<int>(to >> 10);
             auto tv = g_texture8[(ty << 6) + tx];
 
-            to += ts;
+            to -= tst;
+
+            if (tn == 1 && tv > 0) {
+                // dark wall
+                tv >>= 1;
+            }
+            *lb = GetARGB(tv);
+            lb -= SCREEN_WIDTH;
+        }
+
+        to = (32 << 10) + (tst >> 1);
+        lb += sso * SCREEN_WIDTH;
+        for (int y = 0; y < sso; y++) {
+            auto ty = static_cast<int>(to >> 10);
+            auto tv = g_texture8[(ty << 6) + tx];
+
+            to += tst;
 
             if (tn == 1 && tv > 0) {
                 // dark wall
